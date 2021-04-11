ad4b38c0cb889b8560348cf8dbd46aec5c528203
jaridley <jrid88@gmail.com>
2021-04-11 11:32:17 -0500

    Moved entering seed info inside the menu file. Deleted enter_seed_information file.

--- database.py
+++ database.py
@@ -4,39 +4,34 @@ from sqlite3 import Error
 
 def initialize_database():
     try:
-        conn = sqlite3.connect('/Users/jaridley/Database/seed_catalog.db')
-        return conn
+        return sqlite3.connect('/Users/jaridley/Database/seed_catalog.db')
     except Error:
-        print(Error)
+        print(f'executing {Error}')
 
 
-def create_seed_table(conn):
-    with conn:
+def create_seed_table(connect):
+    with connect:
         try:
-            conn.execute()
+            connect.execute()
         except Error:
-            print(Error)
+            print(f'Getting {Error}')
 
 
-def insert_seed_info(conn):
-    with conn:
+def add_seed_info(connect, seed_information):
+    columns = ', '.join("`" + str(x).replace('/', '_') + "`" for x in seed_information.keys())
+    values = ', '.join("'" + str(x).replace('/', '_') + "'" for x in seed_information.values())
+    insert_seed = 'INSERT INTO %s ( %s ) VALUES ( %s );' % ('seed_information', columns, values)
+    with connect:
         try:
-            conn.execute()
+            connect.execute(insert_seed)
+            print(f'Information entered successfully')
         except Error:
-            print(Error)
+            print(f'Info not loading {Error}')
 
 
-def add_seed_info(conn):
-    with conn:
+def get_all_seed_info(connect):
+    with connect:
         try:
-            conn.execute()
-        except Error:
-            print(Error)
-
-
-def get_all_seed_info(conn):
-    with conn:
-        try:
-            conn.execute()
+            connect.execute()
         except Error:
             print(Error)
