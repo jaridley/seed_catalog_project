78bcfeb1fcecf7f6a1aa23ea5e47f58753d86d40
James A Ridley <jrid88@gmail.com>
2021-05-20 10:31:21 -0400

    Added page to add seed data to database.

--- seeds/models.py
+++ seeds/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Seeds(models.Model):
+    seed_name = models.CharField(max_length=50)
+    seed_variety = models.CharField(max_length=50)
+    seed_type = models.CharField(max_length=50)
+    seed_planting_zone = models.CharField(max_length=4)
+    germination_period = models.CharField(max_length=4)
+    days_to_harvest = models.CharField(max_length=4)
+    plant_spacing = models.CharField(max_length=4)
+    row_spacing = models.CharField(max_length=4)
+    plant_depth = models.CharField(max_length=4)
+    sun_requirements = models.CharField(max_length=4)
+    water_requirements = models.CharField(max_length=4)
+
+    def __str__(self):
+        return self.seed_name
--- seeds/views.py
+++ seeds/views.py
@@ -1,5 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Seeds
+from django.contrib import messages
 
 
 def enter_seeds(request):
-    return render(request, 'enter_seeds.html', {})
+    if request.method == 'POST':
+        seed_name = request.POST['seed_name']
+        seed_variety = request.POST['seed_variety']
+        seed_type = request.POST['seed_type']
+        seed_planting_zone = request.POST['seed_planting_zone']
+        germination_period = request.POST['germination_period']
+        days_to_harvest = request.POST['days_to_harvest']
+        plant_spacing = request.POST['plant_spacing']
+        row_spacing = request.POST['row_spacing']
+        plant_depth = request.POST['plant_depth']
+        sun_requirements = request.POST['sun_requirements']
+        water_requirements = request.POST['water_requirements']
+
+        seed_information = Seeds(seed_name=seed_name, seed_variety=seed_variety, seed_type=seed_type,
+                                 seed_planting_zone=seed_planting_zone, germination_period=germination_period,
+                                 days_to_harvest=days_to_harvest, plant_spacing=plant_spacing,
+                                 row_spacing=row_spacing,plant_depth=plant_depth, sun_requirements=sun_requirements,
+                                 water_requirements=water_requirements)
+        seed_information.save()
+        messages.success(request, 'Seed information has been entered successfully.')
+        return redirect('enter_seeds')
+    else:
+        return render(request, 'enter_seeds.html', {})
